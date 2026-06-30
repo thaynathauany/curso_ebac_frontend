c349ac15148651f986a0cc15ebe1c72face63afd
Thayna Thauany <thayna@shockit.com.br>
2026-06-30 15:06:45 -0300

    Entrega parte 3 exercicio

---
+++ Modulo35/eplay-parte-3/src/components/Banner/index.tsx
@@ -0,0 +1,43 @@
+import { Imagem, Titulo, Precos } from './styles'
+
+import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
+import { Game } from '../../pages/Home'
+import { useEffect, useState } from 'react'
+
+import { formataPreco } from '../ProductsList'
+
+import { useGetFeaturedGameQuery } from '../../services/api'
+
+const Banner = () => {
+  const { data: game, isLoading } = useGetFeaturedGameQuery()
+
+  if (!game) {
+    return <h3>Carregando...</h3>
+  }
+
+  return (
+    <Imagem style={{ backgroundImage: `url(${game.media.cover})` }}>
+      <div className="container">
+        <Tag size="big">Destaque do dia</Tag>
+        <div>
+          <Titulo>{game.name}</Titulo>
+          <Precos>
+            De <span>{formataPreco(game.prices.old)}</span> <br />
+            por apenas {formataPreco(game.prices.current)}
+          </Precos>
+        </div>
+        <Button
+          type="link"
+          to={`/product/${game.id}`}
+          title="Clique aqui para aproveitar esta oferta"
+        >
+          Aproveitar
+        </Button>
+      </div>
+    </Imagem>
+  )
+}
+
+export default Banner
---
+++ Modulo35/eplay-parte-3/src/pages/Categories/index.tsx
@@ -0,0 +1,60 @@
+import ProductsList from '../../components/ProductsList'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+import { Game } from '../Home'
+import { useEffect, useState } from 'react'
+
+import {
+  useGetActionGamesQuery,
+  useGetFightGamesQuery,
+  useGetRpgGamesQuery,
+  useGetSimulationGamesQuery,
+  useGetSportGamesQuery
+} from '../../services/api'
+
+const Categories = () => {
+  const { data: actionGames } = useGetActionGamesQuery()
+  const { data: fightGames } = useGetFightGamesQuery()
+  const { data: rpgGames } = useGetRpgGamesQuery()
+  const { data: simulationGames } = useGetSimulationGamesQuery()
+  const { data: sportGames } = useGetSportGamesQuery()
+
+  if (actionGames && fightGames && rpgGames && simulationGames && sportGames) {
+    return (
+      <>
+        <ProductsList
+          games={actionGames}
+          title="Ação"
+          background="black"
+          id="action"
+        />
+        <ProductsList
+          games={sportGames}
+          title="Esportes"
+          background="gray"
+          id="sports"
+        />
+        <ProductsList
+          games={fightGames}
+          title="Luta"
+          background="black"
+          id="fight"
+        />
+        <ProductsList games={rpgGames} title="RPG" background="gray" id="rpg" />
+        <ProductsList
+          games={simulationGames}
+          title="Simulação"
+          background="black"
+          id="simulation"
+        />
+      </>
+    )
+  }
+
+  return <h4>Carregando</h4>
+}
+
+export default Categories
